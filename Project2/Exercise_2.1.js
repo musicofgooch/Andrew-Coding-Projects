6cd8d86c95e079a999df05f83510445b64afa9e7
Andrew Gooch <musicofgooch@gmail.com>
2025-11-15 23:36:42 -0800

    added project 2.1

---
+++ Project2/Exercise_2.1.js
@@ -0,0 +1,6 @@
+const shiftValue = 3; //number
+let location = "GARDEN"; //string
+let safe = true; //boolean
+let encrypted = "";//string
+let decrypted = "";//string
+Number.isInteger(shiftValue); //true
